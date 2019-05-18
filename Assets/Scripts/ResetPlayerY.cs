3dc196a09147a60547ca0178a5f770bf73653d5e
DESKTOP-DQ3OLO0\conor <conorreid1995@hotmail.com>
2019-05-18 04:42:54 +0100

    .

---
+++ Assets/Scripts/ResetPlayerY.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayerY : MonoBehaviour
+{
+    [SerializeField] public Vector3 resetPosition;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        //gameObject.GetComponent<TrailRenderer>().Clear();
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        gameObject.GetComponent<Rigidbody>().transform.position = resetPosition;
+        gameObject.GetComponent<TrailRenderer>().Clear();
+    }
+}
